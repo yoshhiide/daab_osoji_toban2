30abb4a56cc694fa3cdd64c56d601b93f8237a8e
nabeyama yoshihide <yoshihide.nabeyama@l-is-b.com>
2016-09-21 19:42:44 +0900

    メンバー選出ロジックの変更

--- osoji_toban2/scripts/workflow/process/process-update-choose-members.js
+++ osoji_toban2/scripts/workflow/process/process-update-choose-members.js
@@ -8,8 +8,8 @@ const _ = require('lodash');
 const updateChooseMembers = ({ model, workflow }) => {
 
   return ({ domainId, member, chosen, choose }) => {
-    // 選出対象メンバー
-    const targetMembers = _.xor(member, chosen);
+    // 選出対象メンバー（シャッフル）
+    const targetMembers = _.shuffle(_.xor(member, chosen));
 
     // A. 選択不可の場合は空配列を返す
     if ((member.length === 0) || (choose === 0)) {
@@ -26,30 +26,15 @@ const updateChooseMembers = ({ model, workflow }) => {
 
 
     // C. 選出対象メンバー数を判定しながら選出
-    //    0になった場合は、今回選出されなかったメンバーから選出
     let round = false;
-    const chosenObj = {};
-    const targetSize = _.size(targetMembers);
 
-    const chooseMembers = _.range(choose).map(() => {
-      // 未選出メンバーと今回選出メンバー数が同値になった場合、１周したことになる
-      if (targetSize === _.size(chosenObj)) {
+    const chooseMembers = _.range(choose).map((idx) => {
+      // 1巡した場合、今回選出対象でなかったメンバーから選出
+      if (targetMembers.length <= 0) {
         round = true;
-        targetMembers.push(...chosen);
+        targetMembers.push(..._.shuffle(chosen));
       }
-
-      let idx;
-      let unachieved = true;
-      while(unachieved) {
-        // 選出。既に選出済みであれば、繰り返す
-        idx = Math.floor(Math.random() * _.size(targetMembers));
-        if (!chosenObj[idx]) {
-          chosenObj[idx] = true;
-          unachieved = false;
-        }
-      }
-
-      return targetMembers[idx];
+      return targetMembers.pop();
     });
 
     // 選出済み対象者の更新
